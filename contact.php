52b694120b7f08d8c43271d462bf204a66adf58d
Darkcou <38286436+Darkcou@users.noreply.github.com>
2018-04-11 15:34:03 +0200

    php

--- contact.php
+++ contact.php
@@ -3,3 +3,6 @@ require_once('header.php')
 ?>
 <p> il faut simplement signaler dans un premier temps, que le contact peut se faire par téléphone ( 03.20.20.20.20 ) ou par courrier postal (OSSER 45, rue du 11 Novembre 59000 LILLE). Cette partie sera certainement améliorée (le contact par mail par mail par exemple). 
 </p>
+<?php
+require_once('footer.php')
+?>
