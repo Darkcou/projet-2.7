d525e26db56540a3ae1eecf66eb725a60467a6d6
Darkcou <38286436+Darkcou@users.noreply.github.com>
2018-04-11 17:17:23 +0200

    nop

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 require_once('header.php')
 ?>
 <tr>
-	<td><p> il faut simplement signaler dans un premier temps, que le contact peut se faire par téléphone ( 03.20.20.20.20 ) ou par courrier postal (OSSER 45, rue du 11 Novembre 59000 LILLE). Cette partie sera certainement améliorée (le contact par mail par mail par exemple). 
+	<td class="centre" bgcolor="#BBBDFF" height="347" valign="top"><p> il faut simplement signaler dans un premier temps, que le contact peut se faire par téléphone ( 03.20.20.20.20 ) ou par courrier postal (OSSER 45, rue du 11 Novembre 59000 LILLE). Cette partie sera certainement améliorée (le contact par mail par mail par exemple). 
 	</p></td>
 </tr>
 <?php
