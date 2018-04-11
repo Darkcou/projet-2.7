820e159e9e54da9d185731d5cc735826ab280498
eazerfeelds <38286351+eazerfeelds@users.noreply.github.com>
2018-04-11 17:11:07 +0200

    conact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,20 @@
 <?php
 require_once('header.php')
 ?>
-<tr>
-	<td><p> il faut simplement signaler dans un premier temps, que le contact peut se faire par téléphone ( 03.20.20.20.20 ) ou par courrier postal (OSSER 45, rue du 11 Novembre 59000 LILLE). Cette partie sera certainement améliorée (le contact par mail par mail par exemple). 
-	</p></td>
-</tr>
+    <tr>
+        <td class="centre" bgcolor="#BBBDFF" height="347" valign="top">
+            <p><center><h2>Contactez-nous</h2></center>
+            <script language="javascript">
+                function catalogue() {
+                    document.getElementById("sousMenu").style.display = '';
+                }
+            </script>
+            </p><center>
+                <h3> par téléphone ( 03.20.20.20.20 ) <br>ou par courrier postal (OSSER 45, rue du 11 Novembre 59000 LILLE).</h3>
+
+
+            </center></td>
+    </tr>
 <?php
 require_once('footer.php')
 ?>
