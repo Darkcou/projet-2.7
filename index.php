87cc105169641454f406b9b1a2def75791f43f9e
Darkcou <38286436+Darkcou@users.noreply.github.com>
2018-04-11 16:05:54 +0200

    php

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>OSSER</title>
-<link rel="stylesheet" type="text/css" href="style/style.css"></head>
-
-
-<body><center>
-<table width="1016" border="0">
-  <tbody><tr>
-  <td bordercolor="#0000FF" bgcolor="#00B509" class="cadres">
-  <div class="utilisateur" style="float: left; padding-left:0.5em;">Bienvenue chez OSSER</div>
-  <div class="utilisateur" style="float: right; padding-right:0.5em;">N'hésitez pas, contactez nous !!</div>  </td>
-  </tr>
-  <tr>
-  <td width="100%" height="145" nowrap="" background="img/bandeau.jpg" valign="bottom" class="cadres">
-		<script>
-	function surv(menu) {
-		menu.style.background = "url(img/menuOver.png)";
-	}
-	function psurv(menu) {
-		menu.style.background = "url(img/menu.png)";
-	}
-</script>
-
-<div class="menu" id="accueil" style="background-image:URL(img/menuOver.png);"><a href="index.htm" class="lienMenu">Accueil</a></div><div class="menu" id="catalogue" onmouseover="surv(this);" onmouseout="psurv(this);"><a href="catalogue.htm" class="lienMenu">Catalogue</a></div><div class="menu" id="salle" onmouseover="surv(this);" onmouseout="psurv(this);"><a href="lasalle.htm" class="lienMenu">La salle</a></div><div class="menu" id="services" onmouseover="surv(this);" onmouseout="psurv(this);"><a href="services.htm" class="lienMenu">Services</a></div><div class="menu" id="devis" onmouseover="surv(this);" onmouseout="psurv(this);"><a href="devis.htm" class="lienMenu">Devis gratuit</a></div><div class="menu" id="depannage" onmouseover="surv(this);" onmouseout="psurv(this);"><a href="depannage.htm" class="lienMenu">Dépannage</a></div>       </td>   </tr>
+<?php
+require_once('header.php')
+?>
     <tr>
     	<td height="347" bgcolor="#BBBDFF" class="centre" valign="top">
         <p>
